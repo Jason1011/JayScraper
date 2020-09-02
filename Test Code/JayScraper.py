d7feb2356e638c2a6c3ea029573f6a1a07f14c28
Jason1011 <30504104+Jason1011@users.noreply.github.com>
2020-09-02 11:40:29 -0400

    update

--- Test Code/JayScraper.py
+++ Test Code/JayScraper.py
@@ -8,30 +8,6 @@ parser.add_argument('-p','--path', type=str, help='directory path')
 parser.add_argument('-o','--output', type=str, help='If you want the data in a file type file')
 args = parser.parse_args()
 
-#make ouput cooler
-#def download(url):
-
-	#response = urllib.request.urlopen(url)
-	#webContent = response.read()
-
-	#Holder = open('theWebContent.txt','wb')
-	#Holder.write(webContent[0:100000])
-	#Holder.close()
-
-	#pattern = re.compile("</?\w+((\s+\w+(\s*=\s*(?:.*?|'.*?'|[\^'>\s]+))?)+\s*|\s*)/?>")
-
-	#for i, line in enumerate(open('theWebContent.txt')):
-		#for match in re.finditer(pattern, line):
-			#print ('Foundon line %s: %s' % (i+1, match.group()))
-
-	#theType = mimetypes.guess_type(url, strict=True)
-	#Type = open('MIMETYPE.txt','w')
-	#Type.write(str(theType))
-	#Type.close()
-	
-
-
-#download('https://www.mountvernon.org/education/primary-sources-2/article/newburgh-address-george-washington-to-officers-of-the-army-march-15-1783/')
 
 def getWebPage(url, path):
 	
@@ -48,57 +24,7 @@ def getMimeType(url, path, output):
 		info = response.info()
 		mime_type = info.get_content_type()
 	return mime_type
-		#if output == 'file':
-		#	outfile.write("Mimetype: " + info.get_content_type())
-		#else:
-		#	print("Mimetype: " + info.get_content_type())
-	#infilename = "Web_Data.txt"
-	#infile = open(path + "\\" + infilename,'r')
-	#lines = infile.readlines()
-	
-	#if output == 'file':
-	#	outfilename = "MIMEtype_Data.txt"
-	#	outfile = open(path + "\\" + outfilename,'w')
-	#mimetype = False
-	#for line in lines:
-	#	m = re.match('.*((text/plain)|(text/csv)|(text/html)|(image/jpeg)|(image/png)|(image/svg+xml)|(video/mp4)).*', line)
-	#	if m:
-	#		mimetype = m.group(1)
-	#if mimetype:
-	#	if output == 'file':
-	#		outfile.write("MIME type: " + mimetype)
-	#	else:
-	#		print("MIME type: " + mimetype)
-	#else:
-	#	if output == 'file':
-	#		outfile.write("MIME type: text/html")
-	#	else:
-	#		print("MIME type: text/html")	
-
-	#Another way to filer the MIMEtype
-	#Type = open(outfilename,'w')
-	#mimetype = False
-	#flag_last = "FLAG21476147392841"
-	#lines.append(flag_last)
-	#flag_found = False
-	#for line in lines:
-		#m = re.match('.*((text/plain)|(text/csv)|(text/html)|(image/jpeg)|(image/png)|(image/svg+xml)|(video/mp4)).*', line)
-		#if m:
-			#Type.write("MIME type: " + m.group(1))
-			#flag_found = True
-		#elif line == flag_last and flag_found == False:
-			#Type.write("MIME type: text/html")	
-
-#def getElement(element, lines):
-
-#	count = 0
-#	match_string = ".*((<"+element+"+>)|(<"+element+"+)).*"
-#	for line in lines:
-#		t = re.match(match_string, line, re.IGNORECASE)
-#		if t:
-#			if t.group(1) == t.group(1):
-#				count = count + 1
-#	return count
+		
 
 def webData(url, path, output):
 	outer_dict = {}
